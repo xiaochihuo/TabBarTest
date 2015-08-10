af18da59cc5e8d8640e4b8e8ab7fe1ce557019f0
ecar <ecar@ecardeMac-Pro.local>
2015-08-10 13:35:57 +0800

    Initial Commit

---
+++ TabBarTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TabBarTest
+//
+//  Created by ecar on 15/8/10.
+//  Copyright (c) 2015年 张倩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
